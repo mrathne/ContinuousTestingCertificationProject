2b6af06d437e1c1e2de82509cbc677e3abdb0629
mahinda <mrathnayaka@virtusa.com>
2019-12-12 17:20:52 +0530

    ghgh

--- src/test/java/Test/VerifyRegistration.java
+++ src/test/java/Test/VerifyRegistration.java
@@ -1,11 +1,9 @@
 package Test;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -19,10 +17,10 @@ public class VerifyRegistration {
   public void launch(){
 	  	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
-		//options.addArguments("--no-sandbox"); //Bypass OS security model   
-		//options.addArguments("--start-maximized");
-		//options.addArguments("--disable-dev-shm-usage");
-		//options.addArguments("--headless");
+		options.addArguments("--no-sandbox"); //Bypass OS security model   
+		options.addArguments("--start-maximized");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
 		driver = new ChromeDriver(options);
 		driver.get("http://www.newtours.demoaut.com");
 		driver.manage().window().maximize();
@@ -36,7 +34,7 @@ public class VerifyRegistration {
 		driver.findElement(By.name("firstName")).sendKeys("Mahinda");
 		driver.findElement(By.name("lastName")).sendKeys("Rathnayaka");
 		driver.findElement(By.name("phone")).sendKeys("+941234567899");
-		driver.findElement(By.name("userName")).sendKeys("mahinda");
+		driver.findElement(By.name("userName")).sendKeys("abc@gmail.com");
 		driver.findElement(By.name("address1")).sendKeys("address1");
 		driver.findElement(By.name("address2")).sendKeys("address2");
 		driver.findElement(By.name("city")).sendKeys("Colombo");
@@ -44,9 +42,9 @@ public class VerifyRegistration {
 		driver.findElement(By.name("postalCode")).sendKeys("PS CL");
 		Select drpCountry = new Select(driver.findElement(By.name("country")));
 		drpCountry.selectByVisibleText("SRI LANKA");		
-		driver.findElement(By.name("email")).sendKeys("abc@gmail.com");
-		driver.findElement(By.name("password")).sendKeys("Remycru@2");
-		driver.findElement(By.name("confirmPassword")).sendKeys("Remycru@2");
+		driver.findElement(By.name("email")).sendKeys("mahinda");
+		driver.findElement(By.name("password")).sendKeys("mahinda");
+		driver.findElement(By.name("confirmPassword")).sendKeys("mahinda");
 			
 		driver.findElement(By.name("register")).click();
 		
