90a2d9fd81c57e12b8e223db5f5474c82743e3cf
mrathne <56542226+mrathne@users.noreply.github.com>
2019-12-12 17:44:30 +0530

    Update VerifySingin.java

--- src/test/java/Test/VerifySingin.java
+++ src/test/java/Test/VerifySingin.java
@@ -14,7 +14,7 @@ public class VerifySingin {
   
   @BeforeMethod
   public void launch(){
-	  	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	  	System.setProperty("webdriver.chrome.driver", "chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox"); //Bypass OS security model   
 		options.addArguments("--start-maximized");
