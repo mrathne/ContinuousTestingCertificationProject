15669200fb271294d867b0272abf4f846fa3ab3b
mrathne <56542226+mrathne@users.noreply.github.com>
2019-12-12 17:44:04 +0530

    Update VerifyRegistration.java

--- src/test/java/Test/VerifyRegistration.java
+++ src/test/java/Test/VerifyRegistration.java
@@ -15,7 +15,7 @@ public class VerifyRegistration {
   
   @BeforeMethod
   public void launch(){
-	  	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	  	System.setProperty("webdriver.chrome.driver", "chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox"); //Bypass OS security model   
 		options.addArguments("--start-maximized");
